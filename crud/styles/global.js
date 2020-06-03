213ea4e3c74568c1f36acbade1349d5c98dcd05b
RancellTapia <owner@MacBook.local>
2020-06-03 16:05:28 -0400

    Creating Views Components

---
+++ crud/styles/global.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  titulo: {
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 30,
+    fontSize: 30,
+  },
+});
+
+export default globalStyles;
