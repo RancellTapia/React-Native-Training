a6a9af200796b4210cd085b8c3a073690fe3ceb5
RancellTapia <owner@MacBook.local>
2020-07-04 16:03:16 -0400

    [ADD] Starting app
    The app that will connect to the backend created in Apollo Server, GraphQL and MongoDB was created.

---
+++ task_app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
