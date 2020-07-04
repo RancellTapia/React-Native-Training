ac1d34e5f12b8c7bd428f20f4f209c6c7edd27a0
RancellTapia <owner@MacBook.local>
2020-07-04 12:21:44 -0400

    [ADD] Create task in existing projects
    User can add tasks in their created projects.

--- task/db/resolvers.js
+++ task/db/resolvers.js
@@ -1,5 +1,6 @@
 const Usuario = require('../models/Usuarios');
 const Proyecto = require('../models/Proyecto');
+const Tarea = require('../models/Tarea');
 const bcrypyjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
@@ -129,6 +130,18 @@ const resolvers = {
 			await Proyecto.findByIdAndDelete({ _id: id });
 
 			return 'Proyecto Eliminado';
+		},
+
+		nuevaTarea: async (_, { input }, ctx) => {
+			try {
+				const tarea = new Tarea(input);
+				tarea.creador = ctx.usuario.id;
+				const resultado = await tarea.save();
+
+				return resultado;
+			} catch (error) {
+				console.log(error);
+			}
 		},
 	},
 };
