a00a319cd0558588ae9f63d45d511bd76b8a43c9
RancellTapia <owner@MacBook.local>
2020-07-07 18:02:16 -0400

    [ADD] Login component created
    From this component the user can enter their account, it checks the authentication with the database.

--- task_app/App.js
+++ task_app/App.js
@@ -8,6 +8,7 @@ const Stack = createStackNavigator();
 
 import Login from './views/Login';
 import CrearCuenta from './views/CrearCuenta';
+import Proyectos from './views/Proyectos';
 
 const App = () => {
   return (
@@ -29,6 +30,21 @@ const App = () => {
               component={CrearCuenta}
               options={{
                 title: 'Crear Cuenta',
+                headerStyle: {
+                  backgroundColor: '#28303B',
+                },
+                headerTintColor: '#FFF',
+                headerTitleStyle: {
+                  fontWeight: 'bold',
+                },
+              }}
+            />
+
+            <Stack.Screen
+              name="Proyectos"
+              component={Proyectos}
+              options={{
+                title: 'Proyectos',
                 headerStyle: {
                   backgroundColor: '#28303B',
                 },
--- task_app/views/Login.js
+++ task_app/views/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'native-base';
 import {
   Container,
@@ -11,12 +11,74 @@ import {
   Toast,
 } from 'native-base';
 import {useNavigation} from '@react-navigation/native';
-
 import globalStyles from '../styles/global';
+import AsyncStorage from '@react-native-community/async-storage';
+
+//Apollo
+import {gql, useMutation} from '@apollo/client';
+
+const AUTENTICAR_USUARIO = gql`
+  mutation autenticarUsuario($input: AutenticarInput) {
+    autenticarUsuario(input: $input) {
+      token
+    }
+  }
+`;
 
 const Login = () => {
+  //State del formulario
+  const [email, guardarEmail] = useState('');
+  const [password, guardarPassword] = useState('');
+
+  const [mensaje, guardarMensaje] = useState(null);
+
   //React Navigation
   const navigation = useNavigation();
+
+  //Mutation de apollo
+  const [autenticarUsuario] = useMutation(AUTENTICAR_USUARIO);
+
+  //Cuando el ususario presiona en iniciar session
+  const handleSubmit = async () => {
+    //Validar
+    if (email === '' || password === '') {
+      //Mostrar un error
+      guardarMensaje('Todos los campos son obligatorios');
+
+      return;
+    }
+
+    try {
+      //Autenticar Usuario
+      const {data} = await autenticarUsuario({
+        variables: {
+          input: {
+            email,
+            password,
+          },
+        },
+      });
+
+      const {token} = data.autenticarUsuario;
+
+      //Colocar token en storage
+      await AsyncStorage.setItem('token', token);
+
+      //Redireccionar a proyectos
+      navigation.navigate('Proyectos');
+    } catch (error) {
+      guardarMensaje(error.message.replace('GraphQL error: ', ''));
+    }
+  };
+
+  //Muestra mensaje Toast
+  const mostrarAlerta = () => {
+    Toast.show({
+      text: mensaje,
+      buttonText: 'Ok',
+      duration: 6000,
+    });
+  };
 
   return (
     <Container style={[globalStyles.contenedor, {backgroundColor: '#e84347'}]}>
@@ -25,17 +87,28 @@ const Login = () => {
 
         <Form>
           <Item inlineLabel last style={globalStyles.input}>
-            <Input placeholder="Email" />
+            <Input
+              placeholder="Email"
+              onChangeText={(texto) => guardarEmail(texto)}
+            />
           </Item>
         </Form>
 
         <Form>
           <Item inlineLabel last style={globalStyles.input}>
-            <Input secureTextEntry={true} placeholder="Password" />
+            <Input
+              secureTextEntry={true}
+              placeholder="Password"
+              onChangeText={(texto) => guardarPassword(texto)}
+            />
           </Item>
         </Form>
 
-        <Button square block style={globalStyles.boton}>
+        <Button
+          square
+          block
+          style={globalStyles.boton}
+          onPress={() => handleSubmit()}>
           <Text style={globalStyles.botonTexto}>Iniciar Sesion</Text>
         </Button>
 
@@ -44,6 +117,8 @@ const Login = () => {
           style={globalStyles.enlace}>
           Crear Cuenta
         </Text>
+
+        {mensaje && mostrarAlerta()}
       </View>
     </Container>
   );
